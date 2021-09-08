394aed5697d01dfd5d9a0802e02f3c8c91e2667c
Rodney Vlot <99058248@mydavinci.nl>
2021-09-08 13:10:58 +0200

    Eerste commit!

---
+++ person.py
@@ -0,0 +1,18 @@
+print ('Naaminvoer')
+name = input('Geef jouw naam')
+
+print('Adresinvoer')
+adres = input('Geef jouw adres')
+
+print('Postcodeinvoer')
+postcode = input('Geef jouw postcode')
+
+print('Plaatsinvoer')
+plaats = input('Geef jouw woonplaats')
+
+print('----------------------------------------------------')
+print('|  Naam      : ' + name            )
+print('|  Adres     : ' + adres           )
+print('|  Postcode  : ' + postcode           )
+print('|  Woonplaats: ' + plaats          )
+print(' ----------------------------------------------------')
