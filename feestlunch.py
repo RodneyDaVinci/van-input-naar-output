5904ed315edd3f12b7a4b8936ff9c9ae333ee25c
Rodney Vlot <99058248@mydavinci.nl>
2021-09-14 11:03:21 +0200

    Tweede commit!

---
+++ feestlunch.py
@@ -0,0 +1,12 @@
+Crossaintjes = 17
+Stokbrood = 2
+CrossaintPrijs = 0.39
+StokbroodPrijs = 2.78
+Korting = 1.50
+Prijs = 10.69
+
+Centen = int(Prijs * 100)
+
+Uitkomst = "De feestlunch kost je bij de bakker" + str(Centen) + "cent voor de 17 crossaintjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!"
+
+print(Uitkomst)
---
+++ speelhaldag.py
@@ -0,0 +1,10 @@
+Vrienden = 3
+Toegangsticket = 7.45
+PrijsGameSeat = 0.37
+TijdGameSeat = 9
+Prijs = 25.68
+
+Centen = int(Prijs * 100)
+
+Uitkomst = "Dit geweldige dagje uit met 3 mensen in de speelhal met 45 minuten VR kost je maar "  + str(Centen)  +    "cent"
+print(Uitkomst)
